47f6407bfa9fc07178ed3624dc4692649544d129
lorenzobadas <lorenzo.badas@gmail.com>
2024-06-20 22:02:23 +0200

    add instruction generator

---
+++ src/instructions_pkg_generator.py
@@ -0,0 +1,34 @@
+import json
+# open instructions.json
+
+print("library ieee;")
+print("use ieee.std_logic_1164.all;")
+print("package instructions_pkg is")
+opcode_string = "    constant opcode_{}: {}std_logic_vector(5 downto 0) := \"{}\";       -- {}"
+func_string   = "    constant func_{}: {}std_logic_vector(10 downto 0) := \"{}\"; -- {}"
+with open('instructions.json', 'r') as f:
+    instructions = json.load(f)
+    max_name_len = 0
+    for instruction in instructions:
+        # get max length of name field
+        name_len = len(instruction["name"])
+        if name_len > max_name_len:
+            max_name_len = name_len
+    print("    -- OPCODE FIELD")
+    for instruction in instructions:
+        name = instruction["name"]
+        opcode = instruction["opcode"]
+        func = instruction["func"]
+        block = instruction["block"]
+        print(opcode_string.format(name, " "*(max_name_len-len(name)), format(int(opcode, 16), '06b'), opcode))
+    print()
+    print("    -- FUNC FIELD")
+    for instruction in instructions:
+        name = instruction["name"]
+        opcode = instruction["opcode"]
+        func = instruction["func"]
+        block = instruction["block"]
+        if block != "g-i":
+            print(func_string.format(name, " "*(max_name_len-len(name)+(len("opcode")-len("func"))), format(int(func, 16), '011b'), func))
+
+print("end instructions_pkg;")
