2ba92f5154aaaafccea0f5daaf0c0e5bbbe70bf4
lorenzobadas <lorenzo.badas@gmail.com>
2024-06-28 18:41:46 +0200

    add bv_to_string and slv_to_string functions into utils_pkg.vhd

--- src/utils_pkg.vhd
+++ src/utils_pkg.vhd
@@ -6,6 +6,8 @@ use ieee.math_real.all;
 package utils_pkg is
     function clog2 (a: integer) return integer;
     function max (a: integer; b: integer) return integer;
+    function slv_to_string ( a: std_logic_vector) return string;
+    function bv_to_string ( a: bit_vector) return string;
 end package;
 
 package body utils_pkg is
@@ -21,5 +23,23 @@ package body utils_pkg is
         else
             return b;
         end if;
+    end function;
+
+    function slv_to_string ( a: std_logic_vector) return string is
+        variable b : string (a'length-1 downto 1) := (others => NUL);
+    begin
+            for i in a'length-1 downto 1 loop
+            b(i) := std_logic'image(a((i-1)))(2);
+            end loop;
+        return b;
+    end function;
+
+    function bv_to_string ( a: bit_vector) return string is
+        variable b : string (a'length-1 downto 1) := (others => NUL);
+    begin
+            for i in a'length-1 downto 1 loop
+            b(i) := bit'image(a((i-1)))(2);
+            end loop;
+        return b;
     end function;
 end package body;
