ffb9bbc70bb4dc50c1f68390ceeb57673c6adf0c
lorenzobadas <lorenzo.badas@gmail.com>
2024-07-03 20:11:03 +0200

    update reorder buffer architecture

--- src/o3_pkg.vhd
+++ src/o3_pkg.vhd
@@ -4,26 +4,27 @@ use work.utils_pkg.all;
 
 package o3_pkg is
     constant n_reservation_station: integer := 4;
-    constant n_entries_rs: integer := 4;
-    constant n_entries_rob: integer := 16;
+    constant n_entries_rs:          integer := 4;
+    constant n_entries_rob:         integer := 16;
+
     type instruction_t is (jump, branch, load, store, to_reg);
+
     type rob_decoded_instruction is record
         instruction_type: instruction_t;
-        reservation_station: std_logic_vector(clog2(n_reservation_station) downto 0);
-        rs_index: std_logic_vector(clog2(n_entries_rs) downto 0);
-        destination: std_logic_vector(nbit-1 downto 0); -- either register or memory address
+        branch_taken:     std_logic;
+        destination:      std_logic_vector(nbit-1 downto 0); -- either register or memory address
     end record rob_decoded_instruction;
+
     type cdb_t is record
-        result: std_logic_vector(nbit-1 downto 0);
-        reservation_station: std_logic_vector(clog2(n_reservation_station) downto 0);
-        rs_index: std_logic_vector(clog2(n_entries_rs) downto 0);
+        result:       std_logic_vector(nbit-1 downto 0);
+        rob_index:    std_logic_vector(clog2(n_entries_rob)-1 downto 0);
     end record cdb_t;
+    
     type rob_entry is record
-        instruction_type:    instruction_t;
-        result:              std_logic_vector(nbit-1 downto 0);
-        reservation_station: std_logic_vector(clog2(n_reservation_station) downto 0);
-        rs_index:            std_logic_vector(clog2(n_entries_rs) downto 0);
-        destination:         std_logic_vector(nbit-1 downto 0); -- either register or memory address
-        ready:               std_logic; -- ready if result is available
+        instruction_type: instruction_t;
+        result:           std_logic_vector(nbit-1 downto 0);
+        branch_taken:     std_logic;
+        destination:      std_logic_vector(nbit-1 downto 0); -- either register or memory address
+        ready:            std_logic; -- ready if result is available
     end record rob_entry;
 end o3_pkg;
--- src/reorder_buffer.vhd
+++ src/reorder_buffer.vhd
@@ -9,36 +9,38 @@ entity reorder_buffer is
         nbit: integer := 32
     );
     port (
-        clk_i:          in  std_logic;
-        rst_i:          in  std_logic;
-        hazard_i:       in  std_logic; -- stall if high
-        insert_i:       in  std_logic;
-        flush_branch_i: in  std_logic;
-        cdb_i:          in  cdb_t;
-        instruction_i:  in  rob_decoded_instruction;
-        full_o:         out std_logic;
-        destination_o:  out std_logic_vector(nbit-1 downto 0);
-        result_o:       out std_logic_vector(nbit-1 downto 0);
-        mem_write_en_o: out std_logic;
-        reg_write_en_o: out std_logic
+        clk_i:           in  std_logic;
+        rst_i:           in  std_logic;
+        hazard_i:        in  std_logic; -- stall if high
+        insert_i:        in  std_logic;
+        cdb_i:           in  cdb_t;
+        instruction_i:   in  rob_decoded_instruction;
+        full_o:          out std_logic;
+        destination_o:   out std_logic_vector(nbit-1 downto 0);
+        result_o:        out std_logic_vector(nbit-1 downto 0);
+        mem_write_en_o:  out std_logic;
+        reg_write_en_o:  out std_logic;
+        misprediction_o: out std_logic;
+        issue_ptr_o:     out std_logic_vector(clog2(n_entries_rob)-1 downto 0)
     );
 end reorder_buffer;
 
 architecture behav of reorder_buffer is
     type state_t is (idle, full);
-    signal state, state_next: state_t;
     type rob_array is array(0 to n_entries_rob-1) of rob_entry;
-    signal rob_fifo, rob_fifo_next: rob_array;
-    signal head_ptr, head_ptr_next: unsigned(clog2(n_entries_rob)-1 downto 0);
-    signal tail_ptr, tail_ptr_next: unsigned(clog2(n_entries_rob)-1 downto 0);
+    signal state, state_next:           state_t;
+    signal rob_fifo, rob_fifo_next:     rob_array;
+    signal commit_ptr, commit_ptr_next: unsigned(clog2(n_entries_rob)-1 downto 0);
+    signal issue_ptr, issue_ptr_next:   unsigned(clog2(n_entries_rob)-1 downto 0);
 begin
-    comb_proc: process (state, head_ptr, tail_ptr, rob_fifo, branch_fifo, hazard_i, insert_i, flush_branch_i, cdb_i, instruction_i)
+    comb_proc: process (state, commit_ptr, issue_ptr, rob_fifo, branch_fifo, hazard_i, insert_i, flush_branch_i, cdb_i, instruction_i)
         variable rob_address: integer;
         variable commit_this_cycle: boolean := false;
+        variable misprediction_this_cycle: boolean := false;
     begin
         state_next <= state;
-        head_ptr_next <= head_ptr;
-        tail_ptr_next <= tail_ptr;
+        commit_ptr_next <= commit_ptr;
+        issue_ptr_next <= issue_ptr;
         rob_fifo_next <= rob_fifo;
         destination_o <= (others => '0');
         result_o <= (others => '0');
@@ -49,79 +51,89 @@ begin
         else
             full_o <= '1';
         end if;
-        if flush_branch_i = '0' then
-            -- get result from CDB
-            for i in 0 to n_entries_rob-1 loop
-                if cdb_i.reservation_station = rob_fifo(i).reservation_station and
-                   cdb_i.rs_index = rob_fifo(i).rs_index and               --> checks if ID is the same
-                   ((i < tail_ptr and i >= head_ptr) or state = full) then --> checks if the entry is valid
-                    rob_address := i;
+        -- get result from CDB
+        rob_fifo_next(to_integer(unsigned(cdb_i.rob_index))).result <= cdb_i.result;
+        rob_fifo_next(to_integer(unsigned(cdb_i.rob_index))).ready <= '1';
+        -- check if first entry in ROB is ready
+        if rob_fifo(commit_ptr).ready = '1' then
+            if hazard_i = '0' then
+                -- commit result
+                commit_this_cycle := true;
+                destination_o <= rob_fifo(commit_ptr).destination;
+                result_o <= rob_fifo(commit_ptr).result;
+                case rob_fifo(commit_ptr).instruction_type is
+                    when load =>
+                        reg_write_en_o <= '1';
+                    when store =>
+                        mem_write_en_o <= '1';
+                    when others =>
+                        null; -- FIXME
+                end case;
+                mem_write_en_o <= 0; -- FIXME
+                reg_write_en_o <= 0; -- FIXME
+                -- check if branch misprediction
+                if rob_fifo(commit_ptr).instruction_type = branch
+                   and rob_fifo(commit_ptr).branch_taken /= rob_fifo(commit_ptr).result then
+                    -- misprediction
+                    misprediction_this_cycle := true;
+                    -- flush ROB
+                    issue_ptr_next <= commit_ptr + 1;
                 end if;
-            end loop;
-            rob_fifo_next(rob_address).ready <= '1';
-            rob_fifo_next(rob_address).result <= cdb_i.result;
-            -- check if it is a branch or jump instruction
-            if rob_fifo(head_ptr).instruction_type = branch then
-                -- still to be implemented FIXME
-            elsif rob_fifo(head_ptr).instruction_type = jump then
-                -- still to be implemented FIXME
+                commit_ptr_next <= commit_ptr + 1;
             end if;
-            -- check if first entry in ROB is ready
-            if rob_fifo(head_ptr).ready = '1' then
-                if hazard_i = '0' then
-                    -- commit result
-                    commit_this_cycle := true;
-                    destination_o <= rob_fifo(head_ptr).destination;
-                    result_o <= rob_fifo(head_ptr).result;
-                    case rob_fifo(head_ptr).instruction_type is
-                        when load =>
-                            reg_write_en_o <= '1';
-                        when store =>
-                            mem_write_en_o <= '1';
-                        when others =>
-                            null; -- FIXME
-                    end case;
-                    mem_write_en_o <= 0; -- FIXME
-                    reg_write_en_o <= 0; -- FIXME
-                    head_ptr_next <= head_ptr + 1;
-                end if;
-            end if;
+        end if;
+        if not(misprediction_this_cycle) then
             if insert_i = '1' then
-                rob_fifo_next(tail_ptr).instruction_type <= instruction_i.instruction_type;
-                rob_fifo_next(tail_ptr).destination <= instruction_i.destination;
-                rob_fifo_next(tail_ptr).ready <= '0';
-                rob_fifo_next(tail_ptr).reservation_station <= instruction_i.reservation_station;
-                rob_fifo_next(tail_ptr).rs_index <= instruction_i.rs_index;
-                tail_ptr_next <= tail_ptr + 1;
-                if tail_ptr = head_ptr-1 and not(commit_this_cycle) then
+                rob_fifo_next(issue_ptr).instruction_type <= instruction_i.instruction_type;
+                rob_fifo_next(issue_ptr).destination      <= instruction_i.destination;
+                rob_fifo_next(issue_ptr).branch_taken     <= instruction_i.branch_taken;
+                rob_fifo_next(issue_ptr).ready            <= '0';
+                issue_ptr_next <= issue_ptr + 1;
+                if issue_ptr = commit_ptr-1 and not(commit_this_cycle) then
                     state_next <= full;
                 end if;
             end if;
-        else
-            tail_ptr_next <= branch_fifo(0).rob_ptr + 1; -- branch instruction + 1 FIXME
-            -- jump/branch maybe result contains new PC???????
         end if;
     end process comb_proc;
 
     seq_proc: process (clk_i, rst_i)
     begin
         if rst_i = '1' then
-            head_ptr <= (others => '0');
-            tail_ptr <= (others => '0');
+            commit_ptr <= (others => '0');
+            issue_ptr <= (others => '0');
             state <= idle;
         elsif rising_edge(clk_i) then
             state <= state_next;
-            head_ptr <= head_ptr_next;
-            tail_ptr <= tail_ptr_next;
+            commit_ptr <= commit_ptr_next;
+            issue_ptr <= issue_ptr_next;
             rob_fifo <= rob_fifo_next;
             -- still to be implemented
         end if;
     end process seq_proc;
+
+    issue_ptr_o <= issue_ptr;
+    misprediction_o <= '1' when rob_fifo(commit_ptr).instruction_type = branch and 
+                                rob_fifo(commit_ptr).branch_taken /= rob_fifo(commit_ptr).result else
+                       '0';
 end behav;
 
--- Probably there will be a smaller FIFO for the
--- branch instructions.
--- Every entry in the FIFO will contain the ID of
--- the reservation station and the ID of the RS
--- entry, PC of the next instruction, and PC of
--- the branch if it is taken.
+-- ISSUE OPERATION:
+-- issue pointer is used to rename the register
+-- issue pointer is incremented
+-- if issue pointer is equal to commit pointer, then the ROB is full
+
+-- COMMIT OPERATION:
+-- every cycle the instruction pointed by the commit pointer is checked
+-- if the instruction is ready, then the result is committed
+-- if the instruction is a branch and branch_taken flag is different from result, then ROB is emptied (issue_ptr = commit_ptr)
+
+-- CDB OPERATION:
+-- every cycle the CDB is checked
+-- if the CDB has a result for an instruction in the ROB, then the result is written in the ROB and the instruction is marked as ready
+
+-- branch_taken flag is set by the branch prediction unit (BPU) (or simply hardcoded if we are not implementing branch prediction)
+-- result to which branch_taken is compared is the result of the branch instruction, so it comes from the ALU
+
+-- just to be clear, jump instructions do not pose any problem, since they are not speculative
+
+-- in case of misprediction the branch instruction which is committed signals the BPU of the misprediction
