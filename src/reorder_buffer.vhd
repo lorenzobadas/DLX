95a1afba0c4d637331acf36709afb9fee2fca995
Lorenzo Badas <lorenzo.badas@gmail.com>
2024-09-25 17:17:38 +0200

    fix various compiling errors

--- src/reorder_buffer.vhd
+++ src/reorder_buffer.vhd
@@ -34,8 +34,6 @@ entity reorder_buffer is
         -- Branch Unit Interface
         branch_result_o:    out rob_branch_result;
 
-
-
         -- RAT Commit Interface
         commit_register_o: out std_logic
     );
@@ -51,8 +49,8 @@ architecture beh of reorder_buffer is
 
     procedure insert_instruction (
         signal instruction:        in  rob_decoded_instruction;
+        signal issue_ptr:          in  unsigned;
         signal rob_fifo:           out rob_array;
-        signal issue_ptr:          out unsigned;
         signal issue_ptr_next:     out unsigned
     ) is
     begin
@@ -73,22 +71,23 @@ architecture beh of reorder_buffer is
         signal rob_fifo: out rob_array
     ) is
     begin
-        rob_fifo(to_integer(cdb.rob_index)).result <= cdb.result;
-        rob_fifo(to_integer(cdb.rob_index)).ready  <= '1';
+        rob_fifo(to_integer(unsigned(cdb.rob_index))).result <= cdb.result;
+        rob_fifo(to_integer(unsigned(cdb.rob_index))).ready  <= '1';
     end procedure insert_result;
 
     procedure commit_instruction (
         signal rob_fifo:           in  rob_array;
-        signal commit_ptr:         in  unsigned;
+        signal commit_ptr:         in  unsigned(clog2(n_entries_rob)-1 downto 0);
         signal mem_hazard:         in  std_logic;
-        signal commit_ptr_next:    out unsigned;
-        signal issue_ptr_next:     out unsigned;
+        signal commit_ptr_next:    out unsigned(clog2(n_entries_rob)-1 downto 0);
+        signal issue_ptr_next:     out unsigned(clog2(n_entries_rob)-1 downto 0);
         signal state_next:         out state_t;
         signal destination_o:      out std_logic_vector(nbit-1 downto 0);
         signal result_o:           out std_logic_vector(nbit-1 downto 0);
         signal memory_we_o:        out std_logic;
         signal registerfile_we_o:  out std_logic;
-        signal branch_result_o:    out rob_branch_result
+        signal branch_result_o:    out rob_branch_result;
+        signal commit_register_o:  out std_logic
     ) is
     begin
         commit_ptr_next <= commit_ptr + 1;
@@ -107,6 +106,7 @@ architecture beh of reorder_buffer is
                 registerfile_we_o <= '1';
                 destination_o     <= rob_fifo(to_integer(commit_ptr)).destination;
                 result_o          <= rob_fifo(to_integer(commit_ptr)).result;
+                commit_register_o <= '1';
             when branch =>
                 if rob_fifo(to_integer(commit_ptr)).branch_data.branch_taken /= rob_fifo(to_integer(commit_ptr)).result(0) then
                     -- fill branch result info
@@ -131,7 +131,7 @@ begin
     begin
         push       := insert_instruction_i = '1';
         pop        := rob_fifo(to_integer(commit_ptr)).ready = '1' and not(rob_fifo(to_integer(commit_ptr)).instruction_type = store and mem_hazard_i = '1');
-        misp       := pop & (rob_fifo(to_integer(commit_ptr)).instruction_type = branch) & (rob_fifo(to_integer(commit_ptr)).branch_data.branch_taken /= rob_fifo(to_integer(commit_ptr)).result(0));
+        misp       := pop and (rob_fifo(to_integer(commit_ptr)).instruction_type = branch) and (rob_fifo(to_integer(commit_ptr)).branch_data.branch_taken /= rob_fifo(to_integer(commit_ptr)).result(0));
         test_full  := issue_ptr = commit_ptr-1;
         test_empty := issue_ptr = commit_ptr+1;
 
@@ -153,7 +153,7 @@ begin
                 branch_result_o.history       <= (others => '-');
                 branch_result_o.valid         <= '0';
                 if insert_instruction_i = '1' then
-                    insert_instruction(instruction_i, rob_fifo_next, issue_ptr, issue_ptr_next);
+                    insert_instruction(instruction_i, issue_ptr, rob_fifo_next, issue_ptr_next);
                     state_next <= idle;
                 end if;
             when idle =>
@@ -173,7 +173,7 @@ begin
                 branch_result_o.history       <= (others => '-');
                 branch_result_o.valid         <= '0';
                 if insert_instruction_i = '1' then
-                    insert_instruction(instruction_i, rob_fifo_next, issue_ptr, issue_ptr_next);
+                    insert_instruction(instruction_i, issue_ptr, rob_fifo_next, issue_ptr_next);
                 end if;
                 if insert_result_i = '1' then
                     insert_result(cdb_i, rob_fifo_next);
@@ -182,6 +182,7 @@ begin
                     commit_instruction(
                         rob_fifo => rob_fifo,
                         commit_ptr => commit_ptr,
+                        mem_hazard => mem_hazard_i,
                         commit_ptr_next => commit_ptr_next,
                         issue_ptr_next => issue_ptr_next,
                         state_next => state_next,
@@ -189,7 +190,8 @@ begin
                         result_o => result_o,
                         memory_we_o => memory_we_o,
                         registerfile_we_o => registerfile_we_o,
-                        branch_result_o => branch_result_o
+                        branch_result_o => branch_result_o,
+                        commit_register_o => commit_register_o
                     );
                 end if;
                 if push and not(pop) and test_full then
@@ -208,7 +210,6 @@ begin
                 result_o          <= (others => '-');
                 memory_we_o    <= '0';
                 registerfile_we_o    <= '0';
-                misprediction_o   <= '0';
                 commit_register_o <= '0';
                 branch_result_o.branch_taken <= '-';
                 branch_result_o.address      <= (others => '-');
@@ -218,6 +219,7 @@ begin
                     commit_instruction(
                         rob_fifo => rob_fifo,
                         commit_ptr => commit_ptr,
+                        mem_hazard => mem_hazard_i,
                         commit_ptr_next => commit_ptr_next,
                         issue_ptr_next => issue_ptr_next,
                         state_next => state_next,
@@ -225,7 +227,8 @@ begin
                         result_o => result_o,
                         memory_we_o => memory_we_o,
                         registerfile_we_o => registerfile_we_o,
-                        branch_result_o => branch_result_o
+                        branch_result_o => branch_result_o,
+                        commit_register_o => commit_register_o
                     );
                     full_o <= '0';
                 end if;
@@ -259,7 +262,7 @@ begin
             commit_ptr <= (others => '0');
             issue_ptr <= (others => '0');
             for i in 0 to n_entries_rob-1 loop
-                rob_fifo(i).instruction_type <= (others => '-');
+                rob_fifo(i).instruction_type <= to_reg;
                 rob_fifo(i).result           <= (others => '-');
                 rob_fifo(i).destination      <= (others => '-');
                 rob_fifo(i).branch_data.branch_taken   <= '-';
