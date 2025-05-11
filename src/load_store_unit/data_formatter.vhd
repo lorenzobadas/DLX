710fc9d7aac1d65bdeb98f114263393c84cddc9a
Lorenzo Badas <lorenzo.badas@gmail.com>
2025-05-11 11:47:26 +0200

    merged data_formatter.vhd from feature-lsu

--- src/load_store_unit/data_formatter.vhd
+++ src/load_store_unit/data_formatter.vhd
@@ -7,32 +7,36 @@ entity data_formatter is
         nbit: integer := 32
     );
     port (
-        data_in:         in  std_logic_vector(nbit-1 downto 0);
-        last_two_bits_i: in  std_logic_vector(1 downto 0);
-        format_i:        in  std_logic_vector(2 downto 0);
-        data_out:        out std_logic_vector(nbit-1 downto 0)
+        data_in:  in  std_logic_vector(nbit-1 downto 0);
+        offset_i: in  std_logic_vector(1 downto 0);
+        format_i: in  std_logic_vector(2 downto 0);
+        data_out: out std_logic_vector(nbit-1 downto 0)
     );
 end entity;
 
 -- it does not detect misaligned accesses
 architecture beh of data_formatter is
 begin
-    process(data_in, last_two_bits_i, format_i)
-        variable last_two_bits: integer;
+    process(data_in, offset_i, format_i)
+        variable offset_2bits: integer;
+        variable offset_1bit:  integer;
     begin
-        last_two_bits := to_integer(unsigned(last_two_bits_i));
+        offset_2bits := to_integer(unsigned(offset_i));
+        offset_1bit  := to_integer(unsigned(offset_i(1 downto 1)));
         case format_i is
             when "000" => -- signed byte
-                data_out <= std_logic_vector(resize(signed(data_in(last_two_bits*8+7 downto last_two_bits*8)), nbit));
+                data_out <= std_logic_vector(resize(signed(data_in(offset_2bits*8+7 downto offset_2bits*8)), nbit));
             when "001" => -- unsigned byte
-                data_out <= std_logic_vector(resize(unsigned(data_in(last_two_bits*8+7 downto last_two_bits*8)), nbit));
+                data_out <= std_logic_vector(resize(unsigned(data_in(offset_2bits*8+7 downto offset_2bits*8)), nbit));
             when "010" => -- signed halfword
-                data_out <= std_logic_vector(resize(signed(data_in(last_two_bits*16+15 downto last_two_bits*16)), nbit));
+                data_out <= std_logic_vector(resize(signed(data_in(offset_1bit*16+15 downto offset_1bit*16)), nbit));
             when "011" => -- unsigned halfword
-                data_out <= std_logic_vector(resize(unsigned(data_in(last_two_bits*16+15 downto last_two_bits*16)), nbit));
+                data_out <= std_logic_vector(resize(unsigned(data_in(offset_1bit*16+15 downto offset_1bit*16)), nbit));
             when "100" => -- word
                 data_out <= data_in;
             when others =>
+                -- default case, should not happen -> write undefined value
+                data_out <= (others => 'u');
         end case;
     end process;
 end beh;
